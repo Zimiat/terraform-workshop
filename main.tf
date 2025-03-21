d88b240dd16a15d9f7157486fc81ae6d80cf5329
Yason <81364395+Zimiat@users.noreply.github.com>
2025-03-21 13:17:47 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,134 @@
+###############################################################################
+# Terraform Configuration
+###############################################################################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+###############################################################################
+# Generate an SSH Key Pair
+###############################################################################
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+###############################################################################
+# Save Private Key to Local File
+###############################################################################
+resource "local_file" "ssh_private_key_file" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "id_rsa_alma"
+}
+
+###############################################################################
+# Resource Group
+###############################################################################
+resource "azurerm_resource_group" "rg" {
+  name     = "almalinux9-rg"
+  location = "westeurope"
+}
+
+###############################################################################
+# Virtual Network & Subnet
+###############################################################################
+resource "azurerm_virtual_network" "vnet" {
+  name                = "almalinux9-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+###############################################################################
+# Public IP (Static) & Network Interface
+###############################################################################
+resource "azurerm_public_ip" "public_ip" {
+  name                = "almalinux9-publicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "almalinux9-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+###############################################################################
+# AlmaLinux 9 (Gen2) Virtual Machine
+###############################################################################
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "almalinux9-vm"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  size                            = "Standard_B1s"
+  admin_username                  = "azureuser"
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+
+  # Use generated SSH public key
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    name                 = "almalinux9-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # AlmaLinux 9 Gen2 Image Reference
+  source_image_reference {
+    publisher = "almalinux"
+    offer     = "almalinux-x86_64"
+    sku       = "9-gen2"
+    version   = "latest"
+  }
+}
+
+###############################################################################
+# Outputs
+###############################################################################
+# Public IP
+output "public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.public_ip.ip_address
+}
+
+# SSH command - references the saved local key file
+output "ssh_command" {
+  description = "SSH command to connect to the VM"
+  value       = "ssh -i id_rsa_alma azureuser@${azurerm_public_ip.public_ip.ip_address}"
+}
